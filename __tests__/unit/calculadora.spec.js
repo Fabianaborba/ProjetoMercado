456a6acb23a2add98f5ff2f0e7df06ed079bf348
Fabiana Borba <borba-fabiana@hotmail.com>
2022-10-13 21:08:52 -0300

    Incluir o Teste de Subtrair

--- __tests__/unit/calculadora.spec.js
+++ __tests__/unit/calculadora.spec.js
@@ -13,6 +13,7 @@ test("Somar 5 + 7", function () {
     // 1.2 Resultado esperado
     resultadoEsperado = 12;
 
+    // 1.3 configura a função que será somada
     // 2 - Executa
     // Criando um objeto para receber a função calculadora
     const somarDoisNumeros = calculadora.somarDoisNumeros;
@@ -22,4 +23,27 @@ test("Somar 5 + 7", function () {
 
     // 3 - Valida
     expect(resultadoAtual).toBe(resultadoEsperado);
+})
+
+test("Subtrair 15 - 7", () => {
+    // 1 - Configura > Arrange
+    // 1.1 Dados de Entrada
+    const num1 = 15;
+    const num2 = 7;
+
+    // Saídas
+    resultadoEsperado = 8;
+
+    // 2 - Executa > Act
+    const subtrairDoisNumeros = calculadora.subtrairDoisNumeros
+    const resultadoAtual = subtrairDoisNumeros(num1, num2);
+
+    // 3 - Valida > Assert
+    expect(resultadoAtual).toBe(resultadoEsperado);
+
+
+
+
+
+
 })
