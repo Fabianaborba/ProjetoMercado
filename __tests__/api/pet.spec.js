1ceea2a0bf41b03f5b3db800633cfedbc9235ecc
Fabiana Borba <borba-fabiana@hotmail.com>
2022-10-25 22:04:22 -0300

    Atualização pet1

--- __tests__/api/pet.spec.js
+++ __tests__/api/pet.spec.js
@@ -14,6 +14,17 @@ describe("PetStore Swagger - Pet", () => {
     // Post - teste de incluir um animal
     it("Post Pet", () => {
         // Configura
-        
+        const jsonFile = require("../../vendors/json/pet1.json");
+        // Realizar a requisição a receber a resposta
+        return request       // Chamada apra a requisição
+            .post("/pet")    // endpoint / função para incluir o animal
+            .send(jsonFile)  // Envia os dados do animal no corpo da requisição
+            // Valida
+            .then((response) => {                                // pra todo request, há um response
+                assert.equal(response.statusCode, 200);         // Validando status code > se requisição foi recebida e processada
+                assert.equal(response.body.id, petId);       // Se é ID esperado do animal
+                assert.equal(response.body.name, "ScoobyDoo");  // Se é o nome esperado
+                assert.equal(response.body.status, "available");   // Se está com status esperado
+            });
     });
 });
